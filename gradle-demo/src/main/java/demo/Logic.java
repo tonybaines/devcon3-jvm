ff8c5d35cc4801f014841b3f9bc7f377cbc39409
tony <tony@singularity>
2010-09-26 18:36:11 +0100

    savepoint

---
+++ gradle-demo/src/main/java/demo/Logic.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Logic {
+  public boolean and(boolean a, boolean b) {
+    return a && b;
+  }
+  public boolean or(boolean a, boolean b) {
+    return a || b;
+  }
+  public boolean xor(boolean a, boolean b) {
+    return a ^ b;
+  }
+  public boolean not(boolean a) {
+    return !a;
+  }
+}
